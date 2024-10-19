03de4e71b7826409630db6a05dd8bf76e4c0ebed
sabiduria <antdepadoue@gmail.com>
2024-10-19 12:58:31 +0200

    Theme Importation

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,8 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$AppDescription = 'Toleka Console';
+$company = 'Solutic Tech'
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,34 +23,116 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        <?= $cakeDescription ?>:
+        <?= $AppDescription ?>:
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
+    <!-- App css -->
+    <?= $this->Html->css(['bootstrap.min', 'icons.min', 'metisMenu.min', 'app.min',
+        'datatables/dataTables.bootstrap5.min', 'datatables/buttons.bootstrap5.min', 'datatables/responsive.bootstrap4.min',
+        'daterangepicker/daterangepicker']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
+<!-- Left Sidenav -->
+<div class="left-sidenav">
+    <!-- LOGO -->
+    <div class="brand">
+        <?=
+        $this->Html->link('<span>'.$this->Html->image('logo.png', ['class'=>'logo-sm']).'</span>', ['controller'=>''], ['class'=>'logo', 'escape'=>false])
+        ?>
+
+    </div>
+    <!--end logo-->
+    <div class="menu-content h-100" data-simplebar>
+        <?php include ('menu.ctp') ?>
+    </div>
+</div>
+<!-- end left-sidenav-->
+
+
+<div class="page-wrapper">
+    <!-- Top Bar Start -->
+    <div class="topbar">
+        <!-- Navbar -->
+        <nav class="navbar-custom">
+            <ul class="list-unstyled topbar-nav float-end mb-0">
+                <li class="dropdown hide-phone">
+                    <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="false" aria-expanded="false">
+                        <i data-feather="search" class="topbar-icon"></i>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end dropdown-lg p-0">
+                        <!-- Top Search Bar -->
+                        <div class="app-search-topbar">
+                            <form action="#" method="get">
+                                <input type="search" name="search" class="from-control top-search mb-0" placeholder="Type text...">
+                                <button type="submit"><i class="ti-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </li>
+
+                <li class="dropdown">
+                    <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-bs-toggle="dropdown" href="#" role="button"
+                       aria-haspopup="false" aria-expanded="false">
+                        <span class="ms-1 nav-user-name hidden-sm">Username</span>
+                        <?= $this->Html->image('avatar.png', ['class'=>'rounded-circle thumb-xs', 'alt'=>'profile-user']) ?>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <a class="dropdown-item" href="#"><i data-feather="user" class="align-self-center icon-xs icon-dual me-1"></i> Profile</a>
+                        <a class="dropdown-item" href="#"><i data-feather="settings" class="align-self-center icon-xs icon-dual me-1"></i> Settings</a>
+                        <div class="dropdown-divider mb-0"></div>
+                        <a class="dropdown-item" href="#"><i data-feather="power" class="align-self-center icon-xs icon-dual me-1"></i> Logout</a>
+                    </div>
+                </li>
+            </ul><!--end topbar-nav-->
+
+            <ul class="list-unstyled topbar-nav mb-0">
+                <li>
+                    <button class="nav-link button-menu-mobile">
+                        <i data-feather="menu" class="align-self-center topbar-icon"></i>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+        <!-- end navbar-->
+    </div>
+    <!-- Top Bar End -->
+
+    <!-- Page Content-->
+    <div class="page-content">
+
+        <div class="container-fluid">
+
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
+
+        </div><!-- container -->
+
+        <footer class="footer text-center text-sm-start">
+            &copy; <script> document.write(new Date().getFullYear()) </script>
+            <?= $AppDescription ?> <span class="text-muted d-none d-sm-inline-block float-end">Crafted with
+                <i class="mdi mdi-heart text-danger"></i> by <?= $company ?></span>
+        </footer><!--end footer-->
         </div>
-    </main>
-    <footer>
-    </footer>
+        <!-- end page content -->
+        </div>
+        <!-- end page-wrapper -->
+
+
+    <?= $this->Html->script(['jquery.min', 'bootstrap.bundle.min', 'metismenu.min', 'waves', 'feather.min', 'simplebar.min', 'moment',
+        'daterangepicker/daterangepicker',
+        'datatables/jquery.dataTables.min', 'datatables/dataTables.bootstrap5.min', 'datatables/dataTables.buttons.min',
+        'datatables/buttons.bootstrap5.min', 'datatables/jszip.min', 'datatables/pdfmake.min', 'datatables/vfs_fonts',
+        'datatables/buttons.html5.min', 'datatables/buttons.print.min', 'datatables/buttons.colVis.min',
+        'datatables/dataTables.responsive.min', 'datatables/responsive.bootstrap4.min', 'jquery.datatable.init',
+        'app']) ?>
+
 </body>
 </html>
---
+++ templates/layout/menu.ctp
@@ -0,0 +1,22 @@
+<ul class="metismenu left-sidenav-menu">
+    <li class="menu-label mt-0">Main</li>
+    <li>
+        <?= $this->Html->link('<i data-feather="layers" class="align-self-center menu-icon"></i>Affectations', ['controller'=>'affectations', 'action'=>'index'], ['escape'=>false]) ?>
+    </li>
+    <li>
+        <?= $this->Html->link('<i data-feather="layers" class="align-self-center menu-icon"></i>Cobalt Packets', ['controller'=>'cohbags', 'action'=>'index'], ['escape'=>false]) ?>
+    </li>
+    <li>
+        <?= $this->Html->link('<i data-feather="layers" class="align-self-center menu-icon"></i>Copper Packets', ['controller'=>'cubundle', 'action'=>'index'], ['escape'=>false]) ?>
+    </li>
+
+    <hr class="hr-dashed hr-menu">
+    <li class="menu-label my-2">Components & Extra</li>
+
+    <li>
+        <?= $this->Html->link('<i data-feather="layers" class="align-self-center menu-icon"></i>Users', ['controller'=>'users', 'action'=>'index'], ['escape'=>false]) ?>
+    </li>
+    <li>
+        <?= $this->Html->link('<i data-feather="layers" class="align-self-center menu-icon"></i>Settings', ['controller'=>'users', 'action'=>'index'], ['escape'=>false]) ?>
+    </li>
+</ul>
